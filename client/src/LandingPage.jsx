20fc443b8a3b030033d7672e7f4f8c54aad33d1d
Plaban Kumar Mondal <plaban.kr.mondal00@gmail.com>
2022-12-04 02:40:25 +0530

    Add ethers.js code for meta mask account connection

--- client/src/LandingPage.jsx
+++ client/src/LandingPage.jsx
@@ -1,10 +1,18 @@
 import { WorldIDWidget } from "@worldcoin/id";
 import { useEffect, useState } from "react";
+import ABI from "../contracts/OilExploration_ABI.json";
+import ContractArtifact from "../contracts/OilExploration.json";
 import logo from "./assets/logo.png";
 import "./LandingPage.css";
 
 function LandingPage() {
+  // World coin states
   const [verified, setVerified] = useState(false);
+  // Ethers and Meta Mask states
+  const [selectedWalletAddress, setSelectedWalletAddress] = useState("");
+  const [contract, setContract] = useState(null);
+
+  // World coin verification
   const handleVerification = async (verificationResponse) => {
     const options = {
       action_id: "wid_90c20f9fcea5018032f8960046ac8285",
@@ -14,6 +22,7 @@ function LandingPage() {
       merkle_root: verificationResponse.merkle_root,
     };
 
+    // world coin post req
     const res = await fetch("https://developer.worldcoin.org/api/v1/verify", {
       method: "POST",
       headers: {
@@ -29,6 +38,30 @@ function LandingPage() {
     setVerified(true);
   };
 
+  // Handle Meta mask connection button click
+  const CONTRACT_ADDRESS = ContractArtifact.networks["5777"].address;
+
+  const handleMetaBtn = () => {
+    if (window.ethereum && window.ethereum.isMetaMask) {
+      window.ethereum
+        .request({ method: "eth_requestAccounts" })
+        .then((result) => {
+          const _selWaAddress = result[0];
+          setSelectedWalletAddress(_selWaAddress);
+          const _provider = new ethers.providers.Web3Provider(window.ethereum);
+          const _signer = _provider.getSigner();
+          const _contract = new ethers.Contract(CONTRACT_ADDRESS, ABI, _signer);
+          setContract(_contract);
+        })
+        .catch((error) => {
+          setErrorMessage(error.message);
+        });
+    } else {
+      console.log("Need to install MetaMask");
+      setErrorMessage("Please install MetaMask browser extension to interact");
+    }
+  };
+
   return (
     <div className="landing-bg">
       <main className="landing-main">
@@ -36,16 +69,27 @@ function LandingPage() {
           <img src={logo} alt="Logo" />
         </section>
         <section className="world-coin-container">
-          <WorldIDWidget
-            actionId="wid_90c20f9fcea5018032f8960046ac8285"
-            signal="test_signal_01"
-            enableTelemetry
-            onSuccess={(verificationResponse) => {
-              handleVerification(verificationResponse);
-            }}
-            onError={(error) => console.log(error)}
-            debug={true}
-          />
+          {!verified ? (
+            <WorldIDWidget
+              actionId="wid_90c20f9fcea5018032f8960046ac8285"
+              signal="test_signal_01"
+              enableTelemetry
+              onSuccess={(verificationResponse) => {
+                handleVerification(verificationResponse);
+              }}
+              onError={(error) => {
+                console.log(error);
+                if (error.code == "already_signed") {
+                  setVerified(true);
+                }
+              }}
+              debug={true}
+            />
+          ) : (
+            <button id="meta-btn" onClick={handleMetaBtn}>
+              Connect to Meta Mask
+            </button>
+          )}
         </section>
       </main>
     </div>
