f0bc5296ae09fc04eab46f744bbfe2e8eb0015ee
Plaban Kumar Mondal <plaban.kr.mondal00@gmail.com>
2022-12-03 14:18:22 +0530

    Add Worldcoin

--- client/src/LandingPage.jsx
+++ client/src/LandingPage.jsx
@@ -1,3 +1,4 @@
+import { WorldIDWidget } from "@worldcoin/id";
 import logo from "./assets/logo.png";
 import "./LandingPage.css";
 
@@ -8,7 +9,17 @@ function LandingPage() {
         <section>
           <img src={logo} alt="Logo" />
         </section>
-        <section className="world-coin-container"></section>
+        <section className="world-coin-container">
+          <WorldIDWidget
+            // actionId="wid_staging_4f30f07cabc4d7a66dc6517289c658cf"
+            actionId="wid_staging_4f30f07cabc4d7a66dc6517289c658cf"
+            signal="test_signal_01"
+            enableTelemetry
+            onSuccess={(verificationResponse) => console.log(verificationResponse)}
+            onError={(error) => console.log(error)}
+            debug={true}
+          />
+        </section>
       </main>
     </div>
   );
